4e8b814ae3b0742145e690d8f394ad17da432fce
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-10 14:08:51 +0700

    create handler

---
+++ src/api/notes/handler.js
@@ -0,0 +1,102 @@
+class NoteHandler {
+  constructor(service) {
+    this._service = service;
+  }
+
+  postNoteHandler(request, h) {
+    try {
+      const { title = 'untitled', tags, body } = request.payload;
+
+      const noteId = this._service.addNote({ title, tags, body });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
+        data: {
+          noteId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (err) {
+      const response = h.response({
+        status: 'fail',
+        message: err.message,
+      });
+      response.code(400);
+      return response;
+    }
+  }
+
+  getNotesHandler() {
+    const notes = this._service.getNotes();
+
+    return {
+      status: 'success',
+      data: {
+        notes,
+      },
+    };
+  }
+
+  getNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const note = this._service.getNoteById(id);
+      return {
+        status: 'success',
+        data: {
+          note,
+        },
+      };
+    } catch (err) {
+      const response = h.response({
+        status: 'fail',
+        message: err.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  putNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      this._service.editNoteById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
+      };
+    } catch (err) {
+      const response = h.response({
+        status: 'fail',
+        message: err.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+
+  deleteNoteByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      this._service.deleteNoteById(id);
+
+      return {
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
+      };
+    } catch (err) {
+      const response = h.response({
+        status: 'fail',
+        message: err.message,
+      });
+      response.code(404);
+      return response;
+    }
+  }
+}
+
+module.exports = NoteHandler;
