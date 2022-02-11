2f7467ac32112dfee4040b0ee215b06d4dbe6258
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-11 22:49:44 +0700

    implementing database postgres to api

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -12,12 +12,12 @@ class NoteHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', tags, body } = request.payload;
 
-      const noteId = this._service.addNote({ title, tags, body });
+      const noteId = await this._service.addNote({ title, tags, body });
 
       const response = h.response({
         status: 'success',
@@ -49,8 +49,8 @@ class NoteHandler {
     }
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
 
     return {
       status: 'success',
@@ -60,10 +60,10 @@ class NoteHandler {
     };
   }
 
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       return {
         status: 'success',
         data: {
@@ -90,11 +90,11 @@ class NoteHandler {
     }
   }
 
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
-      this._service.editNoteById(id, request.payload);
+      await this._service.editNoteById(id, request.payload);
 
       return {
         status: 'success',
@@ -120,10 +120,10 @@ class NoteHandler {
     }
   }
 
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteNoteById(id);
+      await this._service.deleteNoteById(id);
 
       return {
         status: 'success',
