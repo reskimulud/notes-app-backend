1dd7864a3c6539a683470b23b508f6524902670d
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-11 11:09:32 +0700

    configuring for database and environment

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
@@ -7,8 +9,8 @@ const init = async () => {
   const noteService = new NotesService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
