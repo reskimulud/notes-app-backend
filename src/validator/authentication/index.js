5d90afeb4319302b27511a328c0be6a5d18bbb53
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-02-17 16:20:06 +0700

    create authentications validator

---
+++ src/validator/authentication/index.js
@@ -0,0 +1,32 @@
+const {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
