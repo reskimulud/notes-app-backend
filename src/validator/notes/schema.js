383106c9e524684eaa0b8ce76cc339fccfb6feae
Reski Mulud Muchamad <reski.mulud@gmail.com>
2023-05-17 16:20:05 +0700

    update schema validation

--- src/validator/notes/schema.js
+++ src/validator/notes/schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const NotePayloadSchema = Joi.object({
   title: Joi.string().required(),
   body: Joi.string().required(),
-  tags: Joi.array().items(Joi.string()).required(),
+  tags: Joi.array().items(Joi.string()).allow(null),
 });
 
 module.exports = { NotePayloadSchema };
